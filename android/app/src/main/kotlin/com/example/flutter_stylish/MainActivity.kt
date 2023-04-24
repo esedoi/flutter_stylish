2d7714eae948a58ef5e491f56b66cef019325b6f
paul chen <paul.chen@55688.com.tw>
2023-04-24 23:52:13 +0800

    apply tappay sample code to project

--- android/app/src/main/kotlin/com/example/flutter_stylish/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_stylish/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.flutter_stylish
 
 import android.os.Bundle
+import com.example.test_tappay.PrimeDialog
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import io.flutter.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -8,22 +11,104 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        val CHANNEL = "com.example.channel"
+//
+//        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+//
+//        flutterEngine?.dartExecutor?.let {
+//            MethodChannel(it.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+//                if (call.method == "getNativeString") {
+//                    val message = "Hello from Kotlin!"
+//                    result.success(message)
+//                } else {
+//                    result.notImplemented()
+//                }
+//            }
+//        }
+//    }
+private val CHANNEL = "test_tappay"
 
-        val CHANNEL = "com.example.channel"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+        Log.d(TAG, "configureFlutterEngine")
 
-        flutterEngine?.dartExecutor?.let {
-            MethodChannel(it.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-                if (call.method == "getNativeString") {
-                    val message = "Hello from Kotlin!"
-                    result.success(message)
-                } else {
-                    result.notImplemented()
-                }
-            }
-        }
+        setChannelByCustomDialog(flutterEngine)
+
     }
+
+    fun setChannelByCustomDialog(flutterEngine: FlutterEngine) {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+
+                if (call.method == "tappay") {
+                    Log.d(TAG, "i got u ^.<")
+
+                    val dialog = PrimeDialog(context, object : PrimeDialog.PrimeDialogListener {
+                        override fun onSuccess(prime: String) {
+                            Log.d(TAG, "onSuccess, prime=$prime")
+                            result.success(prime)
+                        }
+
+                        override fun onFailure(error: String) {
+                            Log.d(TAG, "onFailure, error=$error")
+                            result.success(error)
+                        }
+
+                    })
+
+                    dialog.show()
+
+                } else {
+                    Log.d(TAG, "u know nothing ${call.method}")
+
+                    result.error("404", "404", null)
+                }
+
+//                result.error("404", "404", null)
+            }
+
+    }
+
+    fun setChannelByAlertDialog(flutterEngine: FlutterEngine) {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+
+                if (call.method == "tappay") {
+                    Log.d(TAG, "i got u ^.<")
+
+                    val dialog = MaterialAlertDialogBuilder(context)
+                        .setTitle("Dialog")
+                        .setMessage("Write your message here. ....")
+                        .setPositiveButton("Ok") { _, _ ->
+
+                            result.success("Ok got u")
+                        }
+                        .setNegativeButton("Cancel") { _, _ ->
+
+                            result.success("Cancel got u")
+                        }
+
+                    dialog.show()
+
+                } else {
+                    Log.d(TAG, "u know nothing ${call.method}")
+
+                    result.error("404", "404", null)
+                }
+
+//                result.error("404", "404", null)
+            }
+
+    }
+
+    companion object {
+        const val TAG = "anan-android"
+    }
+
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'home/bloc/home_bloc.dart';
@@ -13,6 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    
     return MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
@@ -25,9 +28,85 @@ class MyApp extends StatelessWidget {
             if (constraints.maxWidth >= 601) {
               return HorizontalView();
             } else {
-              return VerticalView();
+              return MyHomePage(title: 'sample code',);
             }
           }),
         ));
   }
 }
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+        
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _tappayMessage,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _inputCreditCard,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  String _tappayMessage = 'init';
+
+  static const platform = MethodChannel('test_tappay');
+
+  Future<void> _inputCreditCard() async {
+    String message;
+
+    try {
+      final String result = await platform.invokeMethod('tappay');
+      message = result;
+    } on PlatformException catch (e) {
+      message = e.message ?? '';
+    }
+
+    setState(() {
+      _tappayMessage = message;
+    });
+  }
+}
+
+
