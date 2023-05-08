17fe7ab625f6f37c7198e474be34b23c5b7e7117
esedoi <esedoi@esedoideMacBook-Air.local>
2023-05-09 01:13:13 +0800

    webRt sound and vedio connect success

--- lib/web_rtc/WebRTC.dart
+++ lib/web_rtc/WebRTC.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'dart:convert';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
-
+//git test
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -18,10 +17,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
-  
+
+  bool isCaller = false;
+
+  bool _isConnecting = false; // 新增一個標誌表示是否正在建立連接
+
   late WebSocketChannel _channel;
   RTCPeerConnection? _peerConnection;
-
 
   @override
   void initState() {
@@ -39,24 +41,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _initWebSocket() {
-  _channel = IOWebSocketChannel.connect('ws://192.168.182.98:8080');
-  _channel.stream.listen((event) {
-    final message = jsonDecode(event);
-    switch (message['type']) {
-      case 'offer':
-        _handleOfferReceived(message['description']);
-        break;
-      case 'answer':
-        _handleAnswerReceived(message['description']);
-        break;
-      case 'candidate':
-        _handleIceCandidateReceived(message['candidate']);
-        break;
-      default:
-        break;
-    }
-  });
-}
+    _channel = IOWebSocketChannel.connect('ws://192.168.20.98:8080');
+    _channel.stream.listen((event) {
+      final message = jsonDecode(event);
+      switch (message['type']) {
+        case 'init':
+          isCaller = message['isCaller']; // 根據從服務器接收到的消息設置角色
+          if (isCaller) {
+            _createOffer(); // 如果客戶端是caller，則主動創建offer
+          }
+          break;
+        case 'offer':
+          if (!isCaller) {
+            // 只有當客戶端不是caller時，才處理接收到的offer
+            _handleOfferReceived(message['description']);
+          }
+          break;
+        case 'answer':
+          if (isCaller) {
+            // 只有當客戶端是caller時，才處理接收到的answer
+            _handleAnswerReceived(message['description']);
+          }
+          break;
+        case 'candidate':
+          _handleIceCandidateReceived(message['candidate']);
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
 
   _initRenderers() async {
     _localRenderer = RTCVideoRenderer();
@@ -64,198 +79,220 @@ class _MyHomePageState extends State<MyHomePage> {
     _remoteRenderer = RTCVideoRenderer();
     await _remoteRenderer.initialize();
 
-   
-     setState(() {
-
-     }); 
+    setState(() {});
   }
-
-  
 
 //第四步：創建本地流
 
-
   _createStream() async {
-  MediaStream stream = await navigator.mediaDevices.getUserMedia({
-    'audio': true,
-    'video': {
-      'facingMode': 'user',
-    },
-  });
+    MediaStream stream = await navigator.mediaDevices.getUserMedia({
+      'audio': true,
+      'video': {
+        'facingMode': 'user',
+      },
+    });
+    print('Video tracks: ${stream.getVideoTracks().length}');
 
-  _localRenderer.srcObject = stream;
+    _localRenderer.srcObject = stream;
 
-  return stream;
-}
+    return stream;
+  }
 
 // 第五步：創建 PeerConnection
-_createPeerConnection() async {
-  const configuration = {
-    'iceServers': [
-      {
-        'urls': ['stun:stun.l.google.com:19302']
-      }
-    ]
-  };
+  _createPeerConnection() async {
+    const configuration = {
+      'iceServers': [
+        {
+          'urls': ['stun:stun.l.google.com:19302']
+        }
+      ]
+    };
 
-  final peerConnection =
-      await createPeerConnection(configuration, <String, dynamic>{});
+    final peerConnection =
+        await createPeerConnection(configuration, <String, dynamic>{});
 
-  peerConnection.addTrack(await _createStream());
+    MediaStream stream = await _createStream();
+    // 添加音頻和視頻軌道
+    stream.getAudioTracks().forEach((track) {
+      peerConnection.addTrack(track, stream);
+    });
+    stream.getVideoTracks().forEach((track) {
+      peerConnection.addTrack(track, stream);
+    });
 
-  return peerConnection;
-}
-
-
-
-
+    return peerConnection;
+  }
 
 // // 第六步：設置事件監聽器
 
+  _setupPeerConnection() async {
+    if (_peerConnection != null) {
+      return _peerConnection;
+    }
 
-_setupPeerConnection() async {
-  if (_peerConnection != null) {
+    _peerConnection = await _createPeerConnection();
+
+    _peerConnection!.onIceCandidate = (candidate) {
+      if (candidate != null) {
+        _channel.sink.add(jsonEncode({
+          'type': 'candidate',
+          'candidate': {
+            'candidate': candidate.candidate,
+            'sdpMid': candidate.sdpMid,
+            'sdpMLineIndex': candidate.sdpMLineIndex,
+          },
+        }));
+      }
+    };
+
+    _peerConnection!.onTrack = (event) {
+      if (event.track.kind == 'video') {
+        setState(() {
+          _remoteRenderer.srcObject = event.streams[0];
+        });
+        // _remoteRenderer.srcObject = event.streams[0];
+      }
+    };
+
+    _peerConnection!.onRemoveStream = (stream) {
+      _remoteRenderer.srcObject = null;
+    };
+
     return _peerConnection;
   }
 
-  _peerConnection = await _createPeerConnection();
-
-  _peerConnection!.onIceCandidate = (candidate) {
-    if (candidate != null) {
-  _channel.sink.add(jsonEncode({
-    'type': 'candidate',
-    'candidate': {
-      'candidate': candidate.candidate,
-      'sdpMid': candidate.sdpMid,
-      'sdpMLineIndex': candidate.sdpMLineIndex,
-    },
-  }));
-}
-  };
-
-  _peerConnection!.onTrack = (event) {
-    if (event.track.kind == 'video') {
-      _remoteRenderer.srcObject = event.streams[0];
-    }
-  }; 
-
-  _peerConnection!.onRemoveStream = (stream) {
-    _remoteRenderer.srcObject = null;
-  };
-
-  return _peerConnection;
-}
-
 // 第七步：創建 Offer
-_createOffer() async {
-  final peerConnection = await _setupPeerConnection();
+  _createOffer() async {
+    if (_isConnecting) return; // 如果正在建立連接，則返回
 
-  final offer = await peerConnection.createOffer();
+    _isConnecting = true; // 設置標誌為true，表示正在建立連接
 
-  await peerConnection.setLocalDescription(offer);
+    setState(() {}); // 更新UI
 
-  
-  
-  _channel.sink.add(jsonEncode({
-    'type': 'offer',
-    'description': offer.toMap(),
-  }));
+    final peerConnection = await _setupPeerConnection();
 
-  // Send offer to remote peer
-}
+    final offer = await peerConnection.createOffer();
 
+    await peerConnection.setLocalDescription(offer);
 
+    _channel.sink.add(jsonEncode({
+      'type': 'offer',
+      'description': offer.toMap(),
+    }));
 
-_handleAnswerReceived(Map<String, dynamic> description) async {
-  final peerConnection = await _setupPeerConnection();
-  await peerConnection.setRemoteDescription(
-    RTCSessionDescription(description['sdp'], description['type']),
-  );
-}
+    // 在成功建立連接後，將標誌設置回false並更新UI
+    _isConnecting = false;
+    setState(() {});
 
-_handleOfferReceived(Map<String, dynamic> description) async {
-  final peerConnection = await _setupPeerConnection();
-  await peerConnection.setRemoteDescription(
-    RTCSessionDescription(description['sdp'], description['type']),
-  );
+    // Send offer to remote peer
+  }
 
-  final answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(answer);
+  _handleAnswerReceived(Map<String, dynamic> description) async {
+    final peerConnection = await _setupPeerConnection();
+    await peerConnection.setRemoteDescription(
+      RTCSessionDescription(description['sdp'], description['type']),
+    );
+  }
 
-  _channel.sink.add(jsonEncode({
-    'type': 'answer',
-    'description': answer.toMap(),
-  }));
-}
+  _handleOfferReceived(Map<String, dynamic> description) async {
+    final peerConnection = await _setupPeerConnection();
+    await peerConnection.setRemoteDescription(
+      RTCSessionDescription(description['sdp'], description['type']),
+    );
 
-_handleIceCandidateReceived(Map<String, dynamic> candidateMap) async {
-  final candidate = RTCIceCandidate(
-    candidateMap['candidate'],
-    candidateMap['sdpMid'],
-    candidateMap['sdpMlineIndex'],
-  );
+    final answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
 
-  final peerConnection = await _setupPeerConnection();
-  await peerConnection.addCandidate(candidate);
-}
+    _channel.sink.add(jsonEncode({
+      'type': 'answer',
+      'description': answer.toMap(),
+    }));
+  }
 
-_createConnection() async {
-  final offer = await _createOffer();
+  _handleIceCandidateReceived(Map<String, dynamic> candidateMap) async {
+    final candidate = RTCIceCandidate(
+      candidateMap['candidate'],
+      candidateMap['sdpMid'],
+      candidateMap['sdpMlineIndex'],
+    );
 
-  // Send offer to remote peer
+    final peerConnection = await _setupPeerConnection();
+    await peerConnection.addCandidate(candidate);
+  }
 
-  // Wait for and handle answer from remote peer
+  _createConnection() async {
+    final offer = await _createOffer();
 
-  // Wait for and handle ICE candidates from remote peer
-}
+    // Send offer to remote peer
 
+    // Wait for and handle answer from remote peer
 
+    // Wait for and handle ICE candidates from remote peer
+  }
 
+  _disconnect() async {
+    if (_peerConnection != null) {
+      await _peerConnection!.close();
+      _peerConnection = null;
+    }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(widget.title),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  key: Key('local'),
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: Colors.black),
-                  // Set a specific width and height for the local video renderer
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  height: MediaQuery.of(context).size.height * 0.45,
-                  child: RTCVideoView(_localRenderer),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  key: Key('remote'),
-                  margin: EdgeInsets.all(10),
-                  decoration: BoxDecoration(color: Colors.black),
-                  // Set a specific width and height for the remote video renderer
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  height: MediaQuery.of(context).size.height * 0.45,
-                  child: RTCVideoView(_remoteRenderer),
-                ),
-              ),
-            ],
-          ),
-          ElevatedButton(
-            onPressed: _createOffer,
-            child: Text('Start WebRTC Connection'),
-          ),
-        ],
+    if (_localRenderer.srcObject != null) {
+      _localRenderer.srcObject!.getTracks().forEach((track) => track.stop());
+      _localRenderer.srcObject = null;
+    }
+
+    if (_remoteRenderer.srcObject != null) {
+      _remoteRenderer.srcObject = null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-    ),
-  );
-}
-
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    key: Key('local'),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(color: Colors.black),
+                    // Set a specific width and height for the local video renderer
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    height: MediaQuery.of(context).size.height * 0.45,
+                    child: RTCVideoView(_localRenderer),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    key: Key('remote'),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(color: Colors.black),
+                    // Set a specific width and height for the remote video renderer
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    height: MediaQuery.of(context).size.height * 0.45,
+                    child: RTCVideoView(_remoteRenderer),
+                  ),
+                ),
+              ],
+            ),
+            ElevatedButton(
+  onPressed: isCaller ? _createOffer : null,
+  child: Text('Start WebRTC Connection as Caller'),
+),
+            ElevatedButton(
+              onPressed: _disconnect,
+              child: Text('Disconnect WebRTC Connection'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
